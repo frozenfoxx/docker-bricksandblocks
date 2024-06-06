c1548ba4dbb72083c41b4531ca7851bc30a490e1
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2024-06-06 11:52:07 -0700

    Updated to add template capability

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # docker-bricksandblocks
+
 Docker deployment files for bricksandblocks.net
+
+# Requirements
+
+* [git](http://git-scm.com)
+* [Task](https://taskfile.dev)
+
+# Configuration
+
+* Make a copy of `env.dist` called `.env`
+* Fill in appropriate values for `.env`
+* Run update and setup tasks
+
+```
+task update
+task setup
+```
+
+# Usage
+
+## Deploy All
+
+To deploy all manifests run:
+
+```
+task deploy
+```
+
+# Contribution
+
+Pull requests welcome.
---
+++ scripts/configure_templates.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Variables
+ROOT_DIR=${ROOT_DIR:-''}
+
+# Functions
+
+## Export variables to replace in the templates
+export_vars()
+{
+    export ROOT_DIR
+}
+
+## Unset variables for safety
+unset_vars()
+{
+    unset ROOT_DIR
+}
+
+## Deploy templates
+deploy_templates()
+{
+    ## Find all .yml.tmpl files in the repository recursively
+    find "${ROOT_DIR}" -type f -name '*.yml.tmpl' | while read -r TMPL_FILE; do
+    
+        # Define the output file name by removing the .tmpl extension
+        YAML_FILE="${TMPL_FILE%.tmpl}"
+    
+        # Use envsubst to replace variables in the .tmpl file and create the .yaml file
+        envsubst < "${TMPL_FILE}" > "${YAML_FILE}"
+    
+        echo "Created ${YAML_FILE}"
+    done
+}
+
+# Logic
+
+export_vars
+deploy_templates
+unset_vars
