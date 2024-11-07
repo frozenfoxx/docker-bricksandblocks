459259207a2970b77194d3dfaac1f9f118e5a031
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2024-11-07 14:04:01 -0800

    Updated deployment stacks

---
+++ scripts/deploy_stack_bittorrent.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Variables
+ROOT_DIR=${ROOT_DIR:-'.'}
+QBITTORRENT_TOR_PORT=""
+
+# Functions
+
+## Deploy stack
+deploy_stack()
+{
+    # Deploy Gluetun VPN
+    docker compose -f compose/gluetun_protonvpn.yml up
+
+    # Check for the forwarded port
+    while [[ ! $(docker exec -it gluetun_protonvpn cat /tmp/gluetun/forwarded_port) ]];
+    do
+      echo "Forwarded port not found, sleeping..."
+      sleep 2
+    done
+
+    # Set forwading port
+    QBITTORRENT_TOR_PORT="$(docker exec -it gluetun_protonvpn cat /tmp/gluetun/forwarded_port)"
+    
+    echo "Port found! Forwarding ${QBITTORRENT_TOR_PORT}, starting BitTorrent..."
+    QBITTORRENT_TOR_PORT="${QBITTORRENT_TOR_PORT}" docker compose -f compose/qbittorrent.yml
+}
+
+# Logic
+
+deploy_stack
